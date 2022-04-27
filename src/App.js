2592ab851f2a4a2b2e61b38c90fa9df0abac2675
vladicK20 <vladvishnevskiy2018@gmail.com>
2022-04-28 00:03:58 +0300

    redux part 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import {useDispatch, useSelector} from "react-redux";
+
 const App = () => {
+    let state = useSelector(state => state);
+    let dispatch = useDispatch()
     return (
         <div>
-
+            <h1>state is - {state}</h1>
+            <button onClick={() => {
+                dispatch({type: 'INC', payload: 1})
+            }}>inc
+            </button>
+            <button onClick={() => {
+                dispatch({type: 'DEC', payload: 1})
+            }}>dec
+            </button>
+            <button onClick={() => {
+                dispatch({type: 'RESET', payload: 0})
+            }}>reset
+            </button>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -2,6 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
+import {Provider} from "react-redux";
+import {createStore} from "redux"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App/>)
+
+function reducer(state = 0, action) {
+    switch (action.type) {
+        case 'INC':
+            console.log(action.payload)
+            return state + action.payload
+        case 'DEC':
+            console.log(action.payload)
+            return state - action.payload
+        case 'RESET':
+            console.log(action.payload)
+            state = action.payload
+            return state
+        default:
+            return state
+    }
+}
+
+let store = createStore(reducer)
+
+root.render(<Provider store={store}>
+    <App/>
+</Provider>)
